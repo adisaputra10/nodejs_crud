ecd4258500f8b6a288f0d07b4c40a028e5930a1e
Adi Saputra <adisaputra.id@gmail.com>
2020-08-09 08:47:42 +0700

    Update conn.js

--- conn.js
+++ conn.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var con = mysql.createConnection({
- host: "127.0.0.1",
- user: "root",
- password: "root",
- database: "ds"
+ host: "localhost",
+ user: "adisaput_db",
+ password: "Qwerty!@#",
+ database: "adisaput_db"
 });
 con.connect(function (err){
  if(err) throw err;
